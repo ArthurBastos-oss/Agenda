302b9b61a8ffa0ad4c7a38235103718dd5fd2fff
Arthur Pereira Bastos <arthurpereirabastos@gmail.com>
2025-10-30 09:11:03 -0300

    Ajuste na verificação

--- Agenda/Svc/SvcTarefa.cs
+++ Agenda/Svc/SvcTarefa.cs
@@ -11,87 +11,44 @@ namespace Agenda.Svc
 {
     internal class SvcTarefa
     {
-        public static bool VerificaTarefa(DateTime dataInicio, DateTime dataFim, Recorrencia recorrencia)
+        public static bool VerificaTarefa(DateTime dataInicio, DateTime dataFim)
         {
             using (OracleConnection conn = new Conexao().AbrirConexao())
             {
-                if (recorrencia == Recorrencia.Nenhuma)
-                { 
-                    using (OracleCommand cmd = new OracleCommand("PKG_Tarefa.VerificaTarefa", conn))
-                    {
-                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
-                        OracleParameter retorno = new OracleParameter("RETURN_VALOR", OracleDbType.Int32);
-                        retorno.Direction = System.Data.ParameterDirection.ReturnValue;
-                        cmd.Parameters.Add(retorno);
-
-                        cmd.Parameters.Add("pDataInicio", OracleDbType.Date).Value = dataInicio;
-                        cmd.Parameters.Add("pDataFim", OracleDbType.Date).Value = dataFim;
-
-                        cmd.ExecuteNonQuery();
-
-                        int count = ((Oracle.ManagedDataAccess.Types.OracleDecimal)retorno.Value).ToInt32();
-
-                        return count > 0;
-                    }
-                }
-                else
+                using (OracleCommand cmd = new OracleCommand("PKG_Tarefa.VerificaTarefa", conn))
                 {
-                    DateTime limite = dataInicio.AddYears(5);
-                    DateTime dataAtual = dataInicio;
-                    List<string> datas = new List<string>();
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
-                    while (dataAtual <= limite)
-                    {
-                        datas.Add(dataAtual.ToString("yyyy-MM-dd"));
+                    OracleParameter retorno = new OracleParameter("RETURN_VALOR", OracleDbType.Int32);
+                    retorno.Direction = System.Data.ParameterDirection.ReturnValue;
+                    cmd.Parameters.Add(retorno);
 
-                        switch (recorrencia)
-                        {
-                            case Recorrencia.Diaria: dataAtual = dataAtual.AddDays(1); break;
-                            case Recorrencia.Semanal: dataAtual = dataAtual.AddDays(7); break;
-                            case Recorrencia.Mensal: dataAtual = dataAtual.AddMonths(1); break;
-                            case Recorrencia.Anual: dataAtual = dataAtual.AddYears(1); break;
-                        }
-                    }
+                    cmd.Parameters.Add("pDataInicio", OracleDbType.Date).Value = dataInicio;
+                    cmd.Parameters.Add("pDataFim", OracleDbType.Date).Value = dataFim;
 
-                    string csvDatas = string.Join(",", datas);
+                    cmd.ExecuteNonQuery();
 
-                    using (OracleCommand cmd = new OracleCommand("PKG_TAREFA.VerificaTarefaRecorrente", conn))
-                    {
-                        cmd.CommandType = CommandType.StoredProcedure;
+                    int count = ((Oracle.ManagedDataAccess.Types.OracleDecimal)retorno.Value).ToInt32();
 
-                        OracleParameter retorno = new OracleParameter("RETURN_VALOR", OracleDbType.Int32)
-                        {
-                            Direction = ParameterDirection.ReturnValue
-                        };
-                        cmd.Parameters.Add(retorno);
-
-                        cmd.Parameters.Add("pDatasCsv", OracleDbType.Varchar2).Value = csvDatas;
-
-                        cmd.ExecuteNonQuery();
-
-                        int count = ((Oracle.ManagedDataAccess.Types.OracleDecimal)retorno.Value).ToInt32();
-                        return count > 0;
-                    }
+                    return count > 0;
                 }
+
             }
         }
 
 
-        //public static int AddTarefa(DateTime dataInicio, DateTime dataFim, string descricao, Recorrencia recorrencia, List<int> contatos = null)
         public static int AddTarefa(List<Tarefa> pTarefas)
         {
             List<string> erros = new List<string>();
-            
+
             foreach (var tarefa in pTarefas)
             {
-                if (VerificaTarefa(tarefa.DataInicio, tarefa.DataFim, tarefa.Recorrencia))
+                if (VerificaTarefa(tarefa.DataInicio, tarefa.DataFim))
                     erros.Add($"Já existe uma tarefa no periodo: {tarefa.DataInicio} a {tarefa.DataFim}");
-                    //throw new Exception("Já existe uma tarefa nesse período. Escolha outro intervalo de tempo.");
 
                 if (tarefa.DataFim < tarefa.DataInicio)
                     erros.Add($"A data de termino da tarefa {tarefa.DataFim} deve vir apos a data de inicio da tarefa {tarefa.DataInicio}");
-                    //throw new Exception("Data de término deve vir após a data de início.");
             }
             if (erros.Count > 0)
             {
@@ -99,9 +56,9 @@ namespace Agenda.Svc
             }
 
             int tarefasCriadas = 0;
-                      
 
-            using (OracleConnection conn = new Conexao().AbrirConexao())           
+
+            using (OracleConnection conn = new Conexao().AbrirConexao())
 
             using (var ts = conn.BeginTransaction())
             {
@@ -132,7 +89,7 @@ namespace Agenda.Svc
 
                             tarefasCriadas = ((Oracle.ManagedDataAccess.Types.OracleDecimal)pResult.Value).ToInt32();
                         }
-                        
+
                     }
                     ts.Commit();
                 }
@@ -143,7 +100,7 @@ namespace Agenda.Svc
                 }
             }
             return tarefasCriadas;
-            
+
         }
 
         public static string CsvListaContatos(List<TarefaContato> listaContatos)
